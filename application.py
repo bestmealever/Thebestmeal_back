89f7a3e7826ad25259a7adbe33ea65359e8f5d73
Lukaid <crescent3859@gmail.com>
2021-10-13 09:39:53 +0900

    test

--- application.py
+++ application.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, jsonify, request
-from pymongo import MongoClient
 import random
 import os
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def main():
     return 'hello'
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    application.run('0.0.0.0', port=5000, debug=True)
